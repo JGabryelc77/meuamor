2d0f06267aae7bfd8398b94c0a8d720664200e1b
João Gabryel Cardoso <joaogabryel1245@gmail.com>
2025-06-17 11:22:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+let highestZ = 1;
+
+class Paper {
+  holdingPaper = false;
+  mouseTouchX = 0;
+  mouseTouchY = 0;
+  mouseX = 0;
+  mouseY = 0;
+  prevMouseX = 0;
+  prevMouseY = 0;
+  velX = 0;
+  velY = 0;
+  rotation = Math.random() * 30 - 15;
+  currentPaperX = 0;
+  currentPaperY = 0;
+  rotating = false;
+
+  init(paper) {
+    const updateMousePosition = (e) => {
+      if (e.cancelable) e.preventDefault(); // ⛔ Impede que o site deslize no celular
+
+      const isTouch = e.type.includes('touch');
+      const clientX = isTouch ? e.touches[0].clientX : e.clientX;
+      const clientY = isTouch ? e.touches[0].clientY : e.clientY;
+
+      if (!this.rotating) {
+        this.mouseX = clientX;
+        this.mouseY = clientY;
+        this.velX = this.mouseX - this.prevMouseX;
+        this.velY = this.mouseY - this.prevMouseY;
+      }
+
+      const dirX = clientX - this.mouseTouchX;
+      const dirY = clientY - this.mouseTouchY;
+      const dirLength = Math.sqrt(dirX * dirX + dirY * dirY);
+      const dirNormalizedX = dirX / dirLength;
+      const dirNormalizedY = dirY / dirLength;
+      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
+      let degrees = 180 * angle / Math.PI;
+      degrees = (360 + Math.round(degrees)) % 360;
+
+      if (this.rotating) {
+        this.rotation = degrees;
+      }
+
+      if (this.holdingPaper) {
+        if (!this.rotating) {
+          this.currentPaperX += this.velX;
+          this.currentPaperY += this.velY;
+        }
+
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+
+        paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
+      }
+    };
+
+    // Eventos de movimento
+    document.addEventListener('mousemove', updateMousePosition);
+    document.addEventListener('touchmove', updateMousePosition, { passive: false });
+
+    const startDrag = (e) => {
+      const isTouch = e.type.includes('touch');
+      const clientX = isTouch ? e.touches[0].clientX : e.clientX;
+      const clientY = isTouch ? e.touches[0].clientY : e.clientY;
+
+      if (this.holdingPaper) return;
+
+      this.holdingPaper = true;
+      paper.style.zIndex = highestZ++;
+      this.mouseTouchX = clientX;
+      this.mouseTouchY = clientY;
+      this.prevMouseX = clientX;
+      this.prevMouseY = clientY;
+
+      if (!isTouch && e.button === 2) {
+        this.rotating = true;
+      }
+    };
+
+    const stopDrag = () => {
+      this.holdingPaper = false;
+      this.rotating = false;
+    };
+
+    // Eventos de início e fim do toque/clique
+    paper.addEventListener('mousedown', startDrag);
+    paper.addEventListener('touchstart', startDrag, { passive: false });
+    window.addEventListener('mouseup', stopDrag);
+    window.addEventListener('touchend', stopDrag);
+  }
+}
+
+const papers = Array.from(document.querySelectorAll('.paper'));
+papers.forEach(paper => {
+  const p = new Paper();
+  p.init(paper);
+});
